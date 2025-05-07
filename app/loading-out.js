d7c352621dd4f554079db0481473cf3a32d73add
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2025-05-07 18:33:57 +0700

    Meal item page

--- app/loading-out.js
+++
@@ -1,9 +0,0 @@
-import classes from "./loading.module.css";
-
-export default function Loading() {
-  return (
-    <p className={classes.loading}>
-      Please wait while we fetch the meals for you.
-    </p>
-  );
-}
--- app/meals/[slug]/page.js
+++
@@ -1,7 +0,0 @@
-export default function MealDetailsPage() {
-  return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meal Detail</h1>
-    </main>
-  );
-}

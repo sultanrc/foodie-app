c4e7b748cea5abe2d2c77923406f3d1bd9b69162
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2025-05-08 13:51:22 +0700

    Share meal

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-      <h1>Page not found!</h1>
-      <p>The page you are looking for does not exist.</p>
+      <h1>Meal not found!</h1>
+      <p>The meal you are looking for does not exist.</p>
     </main>
   );
 }

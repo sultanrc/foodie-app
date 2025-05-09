21e75c58d97b7ca848834b19f5dea26e1acf24e7
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2025-05-09 16:35:13 +0700

    Finishing insert feature

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,13 @@
+"use client";
+
 import { shareMeal } from "@/lib/action";
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
+import FormSubmitButton from "@/components/meals/image-form-submit";
+import { useFormState } from "react-dom";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -20,7 +25,7 @@ export default function ShareMealPage() {
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" />
             </p>
           </div>
           <p>
@@ -41,8 +46,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <FormSubmitButton />
           </p>
         </form>
       </main>
